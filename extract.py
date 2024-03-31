79054187a843b7185a876a97f3cf10d01aa9630c
MarineMehrabyan <103363050+MarineMehrabyan@users.noreply.github.com>
2024-03-31 16:56:39 +0400

    Update extract.py

--- extract.py
+++ extract.py
@@ -20,7 +20,9 @@ def extract_not_blue(source_image, constant_parameter_1 = 84,
                       constant_parameter_3 = 100,
                       constant_parameter_4 = 18):
     img = source_image
+
     img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]  # ensure binary
+
     blobs = img > img.mean()
     blobs_labels = measure.label(blobs, background=1)
     the_biggest_component = 0
@@ -28,26 +30,30 @@ def extract_not_blue(source_image, constant_parameter_1 = 84,
     counter = 0
     average = 0.0
     for region in regionprops(blobs_labels):
+
         if (region.area > 10):
             total_area = total_area + region.area
             counter = counter + 1
         if (region.area >= 250):
             if (region.area > the_biggest_component):
                 the_biggest_component = region.area
+
     average = (total_area/counter)
-    a4_small_size_outliar_constant = ((average/constant_parameter_1)*constant_parameter_2)+constant_parameter_3
+    a4_small_size_outliar_constant = ((average/constant_parameter_1)*
+                                      constant_parameter_2)+constant_parameter_3
     #print("a4_small_size_outliar_constant: " + str(a4_small_size_outliar_constant))
+
     a4_big_size_outliar_constant = a4_small_size_outliar_constant*constant_parameter_4
     #print("a4_big_size_outliar_constant: " + str(a4_big_size_outliar_constant))
+
     pre_version = morphology.remove_small_objects(blobs_labels, a4_small_size_outliar_constant)
     component_sizes = np.bincount(pre_version.ravel())
     too_small = component_sizes > (a4_big_size_outliar_constant)
+    #too_small-ը օգտագործվում է որպես mask
     too_small_mask = too_small[pre_version]
+
     pre_version[too_small_mask] = 0
 
-    #plt.imshow(pre_version)
-    #plt.axis('off')
-    #plt.show()
     plt.imsave('pre_version.png', pre_version)
     img = cv2.imread('pre_version.png', 0)
     img = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
@@ -72,9 +78,6 @@ def extract(image):
         #print("img is not blue")
         img = process_image(image_copy)
         img = extract_not_blue(img)
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
         return img
     else: 
         white_image = np.ones_like(image) * 255
@@ -82,10 +85,6 @@ def extract(image):
         gray_image = cv2.cvtColor(result_image, cv2.COLOR_BGR2GRAY)
         _, thresholded_image = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY_INV)
         inverted_image = cv2.bitwise_not(thresholded_image)
-        #plt.imshow(inverted_image)
-        #plt.axis('off')
-        #plt.show()
+
         return inverted_image
     
-
-
